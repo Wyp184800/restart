ced779384dc066d2f845a9fe51f964c25582e1d1
Wyp184800 <597045367@qq.com>
2015-10-07 18:57:55 +0800

    2015.10.7
    rewrite module div

---
+++ OpenMipsMyDiv/if_id.v
@@ -0,0 +1,33 @@
+`include "defines.v"
+
+module if_id(
+	input		wire						clk,
+	input		wire 						rst,
+	input		wire[5:0]				stall,
+	
+	//来自取指阶段的信号，其中宏定义InstBus表示指令宽度，为32
+	input		wire[`InstAddrBus]	if_pc,
+	input		wire[`InstBus]			if_inst,
+	
+	//对应译码阶段的信号
+	output	reg[`InstAddrBus]		id_pc,
+	output	reg[`InstBus]			id_inst
+);
+
+always	@	(posedge	clk)	begin
+	if(rst == `RstEnable)	begin
+		id_pc		<= `ZeroWord;						//复位时pc为0
+		id_inst 	<= `ZeroWord;						//复位时指令也为0，也就是nop
+	end	else
+	if(stall[1] == `Stop && stall[2] == `NoStop)	begin	//流水线暂停时不传递指令
+		id_pc		<=	`ZeroWord;
+		id_inst	<= `ZeroWord;
+	end	else
+	if(stall[1] == `NoStop)	begin
+		id_pc 	<= if_pc;							//其余时刻向下传递取指阶段的值
+		id_inst  <= if_inst;
+	end
+end
+
+endmodule		
+		
---
+++ OpenMipsMyDiv/pc_reg.v
@@ -0,0 +1,38 @@
+`include "defines.v"
+
+
+module pc_reg(
+	input		wire					clk,
+	input		wire					rst,
+	input		wire[5:0]			stall,//来自控制模块ctrl
+	
+	//转移指令相关
+	input		wire					branch_flag_i,
+	input		wire[`RegBus]		branch_target_address_i,
+	
+	output	reg[`InstAddrBus]	pc,
+	output	reg 					ce
+);
+
+always	@	(posedge clk)	begin
+	if(rst == `RstEnable)	begin
+		ce <= `ChipDisable;				//复位时指令存储器禁用
+	end	else	begin
+		ce <= `ChipEnable;				//复位结束后，指令存储器使能
+	end 
+end
+
+always	@	(posedge clk)	begin
+	if(ce == `ChipDisable)	begin
+		pc <= 32'h00000000;				//指令存储器禁用时，pc为0
+	end	else
+	if(stall[0] == `NoStop)	begin
+		if(branch_flag_i == `Branch)	begin
+			pc	<= branch_target_address_i;
+		end	else	begin
+			pc <= pc + 4'h4;					//指令存储器使能时，pc的值每时钟周期+4
+		end
+	end
+end
+
+endmodule
